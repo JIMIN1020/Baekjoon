f1f3688dff86df595d37ee4a09e7e9eaa6cf9768
Jimin Ryu <121474189+JIMIN1020@users.noreply.github.com>
2023-01-20 16:08:36 +0900

    .

--- Bronze/3003.cpp
+++ Bronze/3003.cpp
@@ -1,20 +1,16 @@
 // [3003] 킹, 퀸, 룩, 비숍, 나이트, 폰
 // 체스는 총 16개의 피스 (킹 1개, 퀸 1개, 룩 2개, 비숍 2개, 나이트 2개, 폰 8개)
 
-#include <iostream>
+#include <stdio.h>
 #define SIZE 6
-using namespace std;
 
 int main(void) {
+    int piece[SIZE] = {1, 1, 2, 2, 2, 8};  // 각 피스별 개수
+    int n;  // 사용자 입력 값을 저장할 변수
     
-    int piece[SIZE] = {1, 1, 2, 2, 2, 8};
-    int n;
-    
-    for (int i = 0; i < SIZE; i++)
-    {
-        cin >> n;
+    for (int i = 0; i < SIZE; i++) {
+        scanf("%d", &n);
         piece[i] -= n;
-        cout << piece[i] << " ";
+        printf("%d ", piece[i]);
     }
 }
- 
