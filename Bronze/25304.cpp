84a7591dceb43e6035ed6eac22e3a3970c121bc5
Jimin Ryu <121474189+JIMIN1020@users.noreply.github.com>
2023-01-20 16:12:32 +0900

    .

--- Bronze/25304.cpp
+++ Bronze/25304.cpp
@@ -1,15 +1,16 @@
+// [25304] 영수증
 #include <stdio.h>
 
 int main(void) {
-    int total, count;  // 총 금액, 개수
-    scanf("%d %d", &total, &count);
-    
-    // 각 가격, 개수 받기
-    for (int i = 0; i < count; i++) {
-        int price, count;
-        scanf("%d %d", &price, &count);
-        total -= price * count;  // 총 금액에서 빼기
+    int X, N;  // 총 금액, 개수
+    scanf("%d %d", &X, &N);
+	   
+		// 각 가격, 개수 받기
+    for (int i = 0; i < N; i++) {
+        int a, b;
+        scanf("%d %d", &a, &b);
+        X -= a * b;  // 총 금액에서 빼기
     }
-    if (total) printf("No\n");
+    if (X) printf("No\n");
     else printf("Yes\n");
 }
