348020ef77125083205c6bb09eba10aae05a18fb
Jimin Ryu <121474189+JIMIN1020@users.noreply.github.com>
2023-01-20 15:55:24 +0900

    .

--- Bronze/2438.cpp
+++ Bronze/2438.cpp
@@ -1,18 +1,15 @@
 // [2438] 별찍기-1
+#include <stdio.h>
 
-#include <iostream>
-using namespace std;
-
-int main()
-{
+int main() {
     int num;
-    cin >> num;
+    scanf("%d", &num);
     
     for (int i = 1; i < num+1; i++) {
         for (int j = 0; j < i; j++)
         {
-            cout << "*";
+            printf("*");
         }
-        cout << endl;
+        printf("\n");
     }
 }
