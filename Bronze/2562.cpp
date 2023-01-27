e21d3eb65adbc44ccfb0fcac4469d313379fb6b4
Jimin Ryu <121474189+JIMIN1020@users.noreply.github.com>
2023-01-27 17:07:19 +0900

    최댓값

---
+++ Bronze/2562.cpp
@@ -0,0 +1,16 @@
+// [2562] 최댓값
+#include <stdio.h>
+
+int main(void) {
+    int max = 0, index;
+    
+    for (int i = 0; i < 9; i++) {
+        int x;
+        scanf("%d", &x);
+        if (max < x) {
+            max = x;
+            index = i+1;
+        }
+    }
+    printf("%d\n%d\n", max, index);
+}
