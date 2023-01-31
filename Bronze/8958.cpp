6f2d50644ca912ce1d79d07e9e4ce07ff01b9a6b
Jimin Ryu <121474189+JIMIN1020@users.noreply.github.com>
2023-01-31 12:38:01 +0900

    OX 퀴즈

---
+++ Bronze/8958.cpp
@@ -0,0 +1,22 @@
+// [8958] OX 퀴즈
+#include <iostream>
+using namespace std;
+
+int main(void) {
+    int N;
+    cin >> N;
+    
+    while (N--) {
+        int sum = 0, ac = 0;
+        string str;
+        cin >> str;
+        for (int i = 0; i < str.length(); i++) {
+            if (str[i] == 'O') {
+                ac += 1;
+                sum += ac;
+            }
+            else ac = 0;
+        }
+        cout << sum << "\n";
+    }
+}
