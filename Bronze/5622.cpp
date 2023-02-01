ac01858279bdd25ac6767806a42d70b41ccc7713
Jimin Ryu <121474189+JIMIN1020@users.noreply.github.com>
2023-02-01 14:21:34 +0900

    다이얼

---
+++ Bronze/5622.cpp
@@ -0,0 +1,15 @@
+// [5622] 다이얼
+#include <iostream>
+using namespace std;
+
+int main(void) {
+    int sum = 0;
+    int dial[] = {3,3,3,4,4,4,5,5,5,6,6,6,7,7,7,8,8,8,8,9,9,9,10,10,10,10};
+    string word;
+    cin >> word;
+    
+    for (int i = 0; i < word.length(); i++) {
+        sum += dial[word[i] - 'A'];
+    }
+    cout << sum << "\n";
+}
