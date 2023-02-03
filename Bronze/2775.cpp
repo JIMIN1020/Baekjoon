7369efa539b5ac0168961b6dd03d3ba615d4f552
Jimin Ryu <121474189+JIMIN1020@users.noreply.github.com>
2023-02-03 13:56:07 +0900

    부녀회장이 될테야

---
+++ Bronze/2775.cpp
@@ -0,0 +1,24 @@
+// [2775] 부녀회장이 될테야
+#include <iostream>
+using namespace std;
+
+int main(void) {
+    int T, k, n;
+    cin >> T;
+    
+    while (T--) {
+        int apart[15][15] = {0};
+        cin >> k >> n;
+        
+        // 0층
+        for (int i = 1; i < n+1; i++) apart[0][i] = i;
+        
+        // 1층 ~ k층
+        for (int i = 1; i < k+1; i++) {
+            for (int j = 1; j < n+1; j++) {
+                apart[i][j] = apart[i-1][j] + apart[i][j-1];
+            }
+        }
+        cout << apart[k][n] << "\n";
+    }
+}
