17ee4ee230f8b4097b3972e0eded0af58bea15d7
Jimin Ryu <121474189+JIMIN1020@users.noreply.github.com>
2023-02-04 23:06:01 +0900

    세로 읽기

---
+++ Bronze/10798.cpp
@@ -0,0 +1,14 @@
+// [10798] 세로읽기
+#include <iostream>
+using namespace std;
+
+int main(void) {
+    char str[5][16] = { 0, };
+    for (int i = 0; i < 5; i++) cin >> str[i];
+    
+    for (int i = 0; i < 15; i++) {
+        for (int j = 0; j < 5; j++) {
+            if (str[j][i] != 0) cout << str[j][i];
+        }
+    }
+}
