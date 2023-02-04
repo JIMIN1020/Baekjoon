971c262a145fd591a098ee839df666bd0a70d1df
Jimin Ryu <121474189+JIMIN1020@users.noreply.github.com>
2023-02-04 23:04:11 +0900

    약수

---
+++ Bronze/1037.cpp
@@ -0,0 +1,12 @@
+// [1037] 약수
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main(void) {
+    int n, arr[51];
+    cin >> n;
+    for (int i = 0; i < n; i++) cin >> arr[i];
+    sort(arr, arr+n);
+    cout << arr[0] * arr[n-1] << "\n";
+}
