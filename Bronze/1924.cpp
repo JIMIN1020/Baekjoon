7940e5b1fda479d574b0f84b3c1ea5d9360142e8
Jimin Ryu <121474189+JIMIN1020@users.noreply.github.com>
2023-02-05 11:32:32 +0900

    2007년

---
+++ Bronze/1924.cpp
@@ -0,0 +1,21 @@
+// [1924] 2007년
+#include <iostream>
+using namespace std;
+
+int main(void) {
+    string day[7] = {"SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"};
+    int last_day[] = {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+    int x, y, sum = 0;
+    cin >> x >> y;
+    
+    // 1~(x-1)월의 모든 일 수 합하기
+    for (int i = 1; i < x; i++) {
+        sum += last_day[i];
+    }
+    
+    // x월의 y일 합하기
+    sum += y;
+    
+    // 일주일(7)로 나눈 나머지를 인덱스로 사용하여 출력
+    cout << day[sum%7] << "\n";
+}
