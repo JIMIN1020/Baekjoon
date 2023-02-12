69ab51978a3f4e8279c5bb018b0f75b0cf8bd2dd
Jimin Ryu <121474189+JIMIN1020@users.noreply.github.com>
2023-02-12 10:38:27 +0900

    주사위 세개

--- Bronze/2480/2480.cpp
+++ Bronze/2480/2480.cpp
@@ -1,24 +1,25 @@
 // [2480] 주사위 세개
-#include <stdio.h>
+#include <iostream>
+using namespace std;
 
 int main(void) {
     
     int a, b, c;  // 주사위 세개
-    scanf("%d %d %d", &a, &b, &c);
+    cin >> a >> b >> c;
     
     // 모두 같을 경우
-    if (a == b && b == c) printf("%d\n", 10000+a*1000);
+    if (a == b && b == c) cout << 10000+a*1000 << "\n";
     
     // 2개가 같을 경우
     else if (a == b || b == c || a == c) {
-        if (a == b || a == c) printf("%d\n", 1000+a*100);
-        else printf("%d\n", 1000+b*100);
+        if (a == b || a == c) cout << 1000+a*100 << "\n";
+        else cout << 1000+b*100 << "\n";
     }
     
     // 모두 다를 경우
     else {
         int d = a > b ? a : b;
         int e = d > c ? d : c;
-        printf("%d\n", e*100);
+        cout << e*100 << "\n";
     }
 }
