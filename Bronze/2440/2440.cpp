6efd1a7b682f175fe05257e88971867889731879
Jimin Ryu <121474189+JIMIN1020@users.noreply.github.com>
2023-02-13 12:03:50 +0900

    별 찍기 - 3

--- Bronze/2440/2440.cpp
+++ Bronze/2440/2440.cpp
@@ -6,9 +6,9 @@ int main(void) {
     int n;
     cin >> n;
     
-    for (int i = 1; i < n+1; i++) {
-        for (int j = n-i; j > 0; j--) cout << " ";
-        for (int k = 1; k < i+1; k++) cout << "*";
+    for (int i = 0; i < n; i++) {
+        for (int j = n-i; j > 0; j--)
+            cout << "*";
         cout << "\n";
     }
 }
