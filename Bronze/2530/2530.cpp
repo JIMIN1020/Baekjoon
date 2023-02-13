bc0bf1cb66989280c685fb2eb65a096d6e61f29f
Jimin Ryu <121474189+JIMIN1020@users.noreply.github.com>
2023-02-13 11:57:16 +0900

    인공지능 시계

--- Bronze/2530/2530.cpp
+++ Bronze/2530/2530.cpp
@@ -1,14 +1,17 @@
 // [2530] 인공지능 시계
-#include <stdio.h>
+#include <iostream>
+using namespace std;
 
 int main(void) {
     int A, B, C, D;
-    scanf("%d %d %d %d", &A, &B, &C, &D);
+    cin >> A >> B >> C >> D;
+    
     C += D;
     B += C / 60;
     C %= 60;
     A += B / 60;
     B %= 60;
     A %= 24;
-    printf("%d %d %d\n", A, B, C);
+    
+    cout << A << " " << B << " " << C << "\n";
 }
