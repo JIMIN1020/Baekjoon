c6f279fdf4a3f3b39b093a8f2aaf6e6b2abb624f
Jimin Ryu <121474189+JIMIN1020@users.noreply.github.com>
2023-02-14 17:10:01 +0900

    세 수

---
+++ Bronze/10817/10817.cpp
@@ -0,0 +1,12 @@
+// [10817] 세 수
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main(void) {
+    int arr[3];
+    cin >> arr[0] >> arr[1] >> arr[2];
+    
+    sort(arr, arr+3);
+    cout << arr[1] << "\n";
+}
