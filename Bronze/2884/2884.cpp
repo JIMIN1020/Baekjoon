b1ac71a44e4b58412180396b031b8c9c8d644c1d
Jimin Ryu <121474189+JIMIN1020@users.noreply.github.com>
2023-02-14 17:03:13 +0900

    알람시계

--- Bronze/2884/2884.cpp
+++ Bronze/2884/2884.cpp
@@ -1,16 +1,16 @@
 // [2884] 알람시계
-#include <stdio.h>
+#include <iostream>
+using namespace std;
 
 int main(void) {
     int h, m;
-    scanf("%d %d", &h, &m);
+    cin >> h >> m;
     
     if (m >= 45) m -= 45;
-    else
-    {
+    else {
         if (h == 0) h = 23;
         else h--;
         m += 15;
     }
-    printf("%d %d\n", h, m);
+    cout << h << " " << m << "\n";
 }
