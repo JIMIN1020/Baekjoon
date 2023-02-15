ce13d6ed2e0a105d92e80823ceb5e72d45524ef8
Jimin Ryu <121474189+JIMIN1020@users.noreply.github.com>
2023-02-15 12:35:15 +0900

    네번째 점

---
+++ Bronze/3009/3009.java
@@ -0,0 +1,16 @@
+// [3009] 네번째 점
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        int x4 = 0, y4 = 0;
+
+        for (int i = 0; i < 3; i++) {
+            x4 ^= sc.nextInt();
+            y4 ^= sc.nextInt();
+        }
+        System.out.println(x4 + " " + y4);
+    }
+}
