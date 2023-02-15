312e523f3b18913db6247fb5f2bed6b4ddd959af
Jimin Ryu <121474189+JIMIN1020@users.noreply.github.com>
2023-02-15 12:35:01 +0900

    네번째 점

--- Bronze/3009.cpp
+++
@@ -1,8 +0,0 @@
-// [3009] 네번째 점
-#include <stdio.h>
-
-int main(void) {
-    int x1, x2, x3, y1, y2, y3;
-    scanf("%d %d %d %d %d %d", &x1, &y1, &x2, &y2, &x3, &y3);
-    printf("%d %d\n", x1^x2^x3, y1^y2^y3);
-}
