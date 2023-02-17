a01b834f0ef465ff5583d00285aeb89067b2778f
Jimin Ryu <121474189+JIMIN1020@users.noreply.github.com>
2023-02-17 13:51:43 +0900

    오븐시계

--- Bronze/2525/2525.cpp
+++ Bronze/2525/2525.cpp
@@ -1,14 +1,14 @@
 // [2525] 오븐시계
-#include <stdio.h>
+#include <iostream>
+using namespace std;
 
-int main(void) {
-    
+int main(void)
+{
     int hour, min, need_t;
-    
-    scanf("%d %d %d", &hour, &min, &need_t);
+    cin >> hour >> min >> need_t;
     
     hour = (hour + (min + need_t) / 60) % 24;
     min = (min + need_t) % 60;
     
-    printf("%d %d\n", hour, min);
+    cout << hour << " " << min;
 }
