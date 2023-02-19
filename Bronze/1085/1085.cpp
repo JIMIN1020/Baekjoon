1169b4a5a44eaf9fd8abf097bfc7226dfbfeed57
Jimin Ryu <121474189+JIMIN1020@users.noreply.github.com>
2023-02-19 21:44:51 +0900

    직사각형에서 탈출하기

--- Bronze/1085/1085.cpp
+++ Bronze/1085/1085.cpp
@@ -1,13 +1,15 @@
 // [1085] 직사각형에서 탈출하기
-#include <stdio.h>
+#include <iostream>
+using namespace std;
 
-int main(void) {
-    int x, y, w, h;
-    scanf("%d %d %d %d", &x, &y, &w, &h);
+int main(void)
+{
+    int x, y, w, h, min;
+    cin >> x >> y >> w >> h;
     
-    int min = 1000;
     min = (w-x) < x ? (w-x) : x;
     min = min < y ? min : y;
     min = min < (h-y) ? min : (h-y);
-    printf("%d\n", min);
+    
+    cout << min << "\n";
 }
