0f13b7ab38a4a38afd7cf2c6b05dbccf1bee7e3f
Jimin Ryu <121474189+JIMIN1020@users.noreply.github.com>
2023-02-20 11:10:55 +0900

    OX 퀴즈

--- Bronze/8958/8958.cpp
+++ Bronze/8958/8958.cpp
@@ -2,17 +2,21 @@
 #include <iostream>
 using namespace std;
 
-int main(void) {
+int main(void) 
+{
     int N;
     cin >> N;
     
-    while (N--) {
+    while (N--) 
+		{
         int sum = 0, ac = 0;
         string str;
         cin >> str;
-        for (int i = 0; i < str.length(); i++) {
+
+        for (int i = 0; i < str.length(); i++) 
+				{
             if (str[i] == 'O') {
-                ac += 1;
+                ac += 1;  // 누적합
                 sum += ac;
             }
             else ac = 0;
