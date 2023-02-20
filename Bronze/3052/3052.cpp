f950e9dda2e6514624c2e996aac9f09c95668b18
Jimin Ryu <121474189+JIMIN1020@users.noreply.github.com>
2023-02-20 11:13:52 +0900

    나머지

--- Bronze/3052/3052.cpp
+++ Bronze/3052/3052.cpp
@@ -2,10 +2,12 @@
 #include <iostream>
 using namespace std;
 
-int main(void) {
+int main(void) 
+{
     int arr[42] = {0}, n, count = 0;
     
-    for (int i = 0; i < 10; i++) {
+    for (int i = 0; i < 10; i++) 
+		{
         cin >> n;
         if (arr[n%42] == 0) {
             arr[n%42] = 1;
