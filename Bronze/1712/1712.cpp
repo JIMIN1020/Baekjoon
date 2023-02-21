8d8a281cff1233aac2a46a1241fdb28706261c1d
Jimin Ryu <121474189+JIMIN1020@users.noreply.github.com>
2023-02-21 14:11:37 +0900

    손익분기점

--- Bronze/1712/1712.cpp
+++ Bronze/1712/1712.cpp
@@ -2,9 +2,11 @@
 #include <iostream>
 using namespace std;
 
-int main(void) {
+int main(void) 
+{
     long long A, B, C;
     cin >> A >> B >> C;
+
     if (B >= C)
         cout << "-1\n";
     else
