c2f2a5a563ce7d6073a65aa6cdd40004285f3788
Jimin Ryu <121474189+JIMIN1020@users.noreply.github.com>
2023-02-21 14:15:51 +0900

    상수

--- Bronze/2908/2908.cpp
+++ Bronze/2908/2908.cpp
@@ -2,11 +2,16 @@
 #include <iostream>
 using namespace std;
 
-int main(void) {
+int main(void) 
+{
     int A, B;
     cin >> A >> B;
+
     A = A%100%10*100 + A%100/10*10 + A/100;
     B = B%100%10*100 + B%100/10*10 + B/100;
-    if (A > B) cout << A << "\n";
-    else cout << B << "\n";
+
+    if (A > B) 
+			cout << A << "\n";
+    else 
+			cout << B << "\n";
 }
