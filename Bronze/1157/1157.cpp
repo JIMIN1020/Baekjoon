2064e2b4a6c11cf34c73195a3d70c2285ad92006
Jimin <121474189+JIMIN1020@users.noreply.github.com>
2023-02-25 23:10:20 +0900

    단어 공부

--- Bronze/1157/1157.cpp
+++ Bronze/1157/1157.cpp
@@ -2,17 +2,19 @@
 #include <iostream>
 using namespace std;
 
-int main(void) {
+int main(void)
+{
     string word;
     cin >> word;
-    int max = 0, check = 0, index = 0, count[60] = {0};
+    int max = 0, index = 0, count[30] = {0};
     
     // 알파벳 수 카운트 하기
     for (int i = 0; i < word.length(); i++)
         count[(word[i] - 'A') % 32] += 1;
     
     // 최댓값 찾기
-    for (int j = 0; j < 26; j++) {
+    for (int j = 0; j < 26; j++)
+    {
         if (count[j] > max) {
             max = count[j];
             index = j;
@@ -21,9 +23,13 @@ int main(void) {
     
     // 최댓값 중복 찾기
     for (int j = 0; j < 26; j++)
-        if (j != index && count[j] == max) check = 1;
+    {
+        if (j != index && count[j] == max) {
+            cout << "?\n";
+            return 0;
+        }
+    }
     
     // 출력
-    if (check == 0) cout << (char)('A' + index) << "\n";
-    else cout << "?\n";
+    cout << (char)('A' + index) << "\n";
 }
