cc3044a10d4e168749583c98ea7be70dd96600a3
Jimin <121474189+JIMIN1020@users.noreply.github.com>
2023-02-25 23:12:02 +0900

    평균은 넘겠지

--- Bronze/4344/4344.cpp
+++ Bronze/4344/4344.cpp
@@ -2,28 +2,35 @@
 #include <iostream>
 using namespace std;
 
-int main(void) {
+int main(void)
+{
     cout << fixed;
     cout.precision(3);  // 소수점 이하 3자리
     
-    int C, N, score[10001];
+    int C, N;
     cin >> C;
     
-    while (C--) {
+    while (C--)
+    {
         cin >> N;
-        double P = 0, avg = 0;
+        int* score = new int[N];
+        
+        // pass: 평균을 넘는 학생들 / avg: 평균
+        double pass = 0, avg = 0;
         
         // 입력 받고 누적합
         for (int i = 0; i < N; i++) {
             cin >> score[i];
             avg += score[i];
         }
+        avg /= N;   // 평균 구하기
         
         // 평균보다 높은 점수 찾기
         for (int j = 0; j < N; j++)
-            if (score[j] > avg / N) P++;
+            if (score[j] > avg) pass++;
         
         // 비율 구해서 출력
-        cout << P / N * 100 << "%\n";
+        cout << pass / N * 100 << "%\n";
+        delete[] score;
     }
 }
