804cd854bb1261d9c4749bfdd4ba12dd1605a862
Jimin <121474189+JIMIN1020@users.noreply.github.com>
2023-02-26 22:04:42 +0900

    소인수분해

--- Bronze/11653/11653.cpp
+++ Bronze/11653/11653.cpp
@@ -2,17 +2,22 @@
 #include <iostream>
 using namespace std;
 
-int main(void) {
+int main(void)
+{
     int N;
     cin >> N;
+    
     if (N == 1) return 0;
 
-    while (N > 1) {
+    while (N > 1)
+    {
+        // 2부터 나누어 떨어지는지 확인
         for (int i = 2; i <= N; i++) {
+            // 나누어 떨어지면?
             if (N % i == 0) {
                 N /= i;
                 cout << i << "\n";
-                break;
+                break;  // 나누고 i를 출력한 뒤 다시 반복문 시작
             }
         }
     }
