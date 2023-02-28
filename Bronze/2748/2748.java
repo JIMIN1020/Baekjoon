2d9d06c55c5203c9da376d3d741f2707d6d99473
Jimin <121474189+JIMIN1020@users.noreply.github.com>
2023-02-28 14:26:57 +0900

    피보나치 수 2

---
+++ Bronze/2748/2748.java
@@ -0,0 +1,22 @@
+// [2748] 피보나치 수 2
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        // 초기값 : 0번째, 1번째 수
+        long a = 0, b = 1;
+
+        for (int i = 0; i < n-1; i++) {
+            // b에 a+b값 넣고 기존의 b 값을 a로 옮기는 과정
+            long temp = b;
+            b += a;
+            a = temp;
+        }
+        System.out.println(b);
+    }
+}
