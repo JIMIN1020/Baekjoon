951ef11c3c1b5fd0b5d02bb5d2bcedd25040f8c7
Jimin <121474189+JIMIN1020@users.noreply.github.com>
2023-02-28 14:30:03 +0900

    약수

--- Bronze/1037/1037.cpp
+++ Bronze/1037/1037.cpp
@@ -3,10 +3,14 @@
 #include <algorithm>
 using namespace std;
 
-int main(void) {
+int main(void)
+{
     int n, arr[51];
     cin >> n;
-    for (int i = 0; i < n; i++) cin >> arr[i];
+    
+    for (int i = 0; i < n; i++)
+        cin >> arr[i];
+    
     sort(arr, arr+n);
     cout << arr[0] * arr[n-1] << "\n";
 }
