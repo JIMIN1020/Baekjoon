822a1e591b91e8dbed9e2fe4ed99e7f3ebd8d6b5
Jimin <121474189+JIMIN1020@users.noreply.github.com>
2023-02-28 14:26:43 +0900

    피보나치 수 2

--- Bronze/2748/2748.cpp
+++ Bronze/2748/2748.cpp
@@ -2,12 +2,16 @@
 #include <iostream>
 using namespace std;
 
-int main(void) {
+int main(void)
+{
     long long n;
     cin >> n;
     
+    // 초기값 : 0번째, 1번째 수
     long long a = 0, b = 1;
+    
     for (int i = 0; i < n-1; i++) {
+        // b에 a+b값 넣고 기존의 b 값을 a로 옮기는 과정
         long long temp = b;
         b += a;
         a = temp;
