0c70e5a21392b78f3013f8654f583eaf9faa7835
Jimin <121474189+JIMIN1020@users.noreply.github.com>
2023-03-01 14:30:08 +0900

    단어 뒤집기

--- Bronze/9093/9093.cpp
+++ Bronze/9093/9093.cpp
@@ -3,27 +3,28 @@
 #include <stack>
 using namespace std;
 
-int main(void) {
-    int T;
+int main(void)
+{
+    int T;s
     cin >> T;
     cin.ignore();       // 입력 버퍼 비우기 (T 입력 후 남은 '\n' 제거)
     stack <char> word;  // 스택 생성
     
-    while (T--) {
+    while (T--)
+    {
         string str;
         getline(cin, str);  // 한 줄 읽어오기
         str += ' ';         // 마지막에 공백 추가 (마지막 단어 확인용)
         
-        for (int i = 0; i < str.length(); i++)
-        {
+        for (int i = 0; i < str.length(); i++) {
             if (str[i] == ' ')  // 공백이면?
             {
                 // 스택에 push 해놓은 단어들 꺼내오기
-                while (word.empty() == false) {
+                while (!word.empty()) {
                     cout << word.top();
                     word.pop();
                 }
-                cout << str[i];      // 공백 출력 (띄어쓰기)
+                cout << ' ';      // 공백 출력 (띄어쓰기)
             }
             else word.push(str[i]);  // 공백 아닐 경우엔 스택에 push
         }
