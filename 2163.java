77eb2b5f4a765ed1a4247ade585a8ee5d5c2d150
JIMIN1020 <jimins4920@gmail.com>
2023-03-02 11:52:49 +0900

    초콜릿 자르기

---
+++ 2163.java
@@ -0,0 +1,16 @@
+// [2163] 초콜릿 자르기
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] s = br.readLine().split(" ");
+
+        int N = Integer.parseInt(s[0]);
+        int M = Integer.parseInt(s[1]);
+
+        System.out.println((N-1) + (M-1)*N);
+    }
+}
