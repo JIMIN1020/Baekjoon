cf756b44f1b9a849d91643dfdcb3e902f90995ea
JIMIN1020 <jimins4920@gmail.com>
2023-03-07 11:48:49 +0900

    소트 인사이드

---
+++ 1427.java
@@ -0,0 +1,21 @@
+// [1427] 소트 인사이드
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Collections;
+
+public class Main {
+    public static void main(String[] args) throws IOException
+    {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] s = br.readLine().split("");
+
+        // 내림차순 정렬
+        Arrays.sort(s, Collections.reverseOrder());
+
+        for (int i = 0; i < s.length; i++)
+            System.out.print(s[i]);
+        System.out.println();
+    }
+}
